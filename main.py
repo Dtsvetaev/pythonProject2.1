21e84e3a0253a4d66965aa6235439e4ca3a0f0b9
Dimitrii Tsvetaev <Dtsvetaev@mail.ru>
2023-11-21 15:51:14 +0300

    Hello world

---
+++ main.py
@@ -0,0 +1,9 @@
+
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import datetime
+
+if __name__ == '__main__':
+    print(f"Today's date: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    calculate_salary()
+    get_employees()
